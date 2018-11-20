47e3c9fadcfa57ee180ea9fae1e1862518f90849
anhev <anhev@andy>
2018-11-20 12:40:05 +0100

    Edited class name of myClass to MyClass. Added Class MyOtherClass.

---
+++ testprojekt/src/testprojekt/MyClass.java
@@ -0,0 +1,13 @@
+package testprojekt;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		MyOtherClass otherClass = new MyOtherClass();
+		
+		otherClass.talk();
+
+	}
+
+}
---
+++ testprojekt/src/testprojekt/MyOtherClass.java
@@ -0,0 +1,8 @@
+package testprojekt;
+
+public class MyOtherClass {
+
+	public void talk() {
+		System.out.println("Hello World!");
+	}
+}
